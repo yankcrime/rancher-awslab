b6a306a896422ffb3a5709015b813b1301592ed1
Nick Jones <nick@dischord.org>
2020-09-16 16:16:30 +0100

    Revert back to sourcing module from GitHub

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "rancher_infra" {
-  source = "../tf-module-rancher-infra-aws"
+  source = "github.com/yankcrime/tf-module-rancher-infra-aws"
 
   aws_access_key       = var.aws_access_key
   aws_secret_key       = var.aws_secret_key
